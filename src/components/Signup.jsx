17016c0dc9affcccae8e29819ae58330628a5b31
Divyang Chavda <chavdadivyang7373@gmail.com>
2025-02-16 12:32:13 +0530

    Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios'
-import '../style/signup.css';
+import '././style/signup.css'
 
 function Signup(){
     const [name,setName]=useState()
