3904158342e32da193db28ce1c457a7e1fba311e
Divyang Chavda <chavdadivyang7373@gmail.com>
2025-02-16 17:23:52 +0530

    Signup.css change path

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,9 +1,7 @@
 import {React,useState}from 'react';
 import '../style/admin.css';
-
 import Adminheader from './Adminheader.jsx';
 import Adminpanel from '././Adminpanel.jsx';
-
 import Adminperform from '././Adminperform.jsx';
 
 function Admin() {
@@ -18,15 +16,8 @@ function Admin() {
              <div className='admin-body'>
             <Adminpanel setselectedItem={setselectedItem}  showPanel={showPanel} search={search} />
             <Adminperform  selectedItems={selectedItems} />
-             </div>
-            
-
-          
-        </div>
-        
+             </div>      
+        </div>      
     )
-      
-    
-
 }
 export default Admin;
